58b3340300aadfe7eacb31f1c6b40163bb2aedba
Sadnan Saquif <ssaquif@gmail.com>
2021-07-29 17:13:41 -0400

    1. dotenv, monk, yup, annoid packages added 2. env varibale added 3. connection to local mongodb established 4. basic url creation enpoint started

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
+const yup = require("yup");
+const { nanoid } = require("nanoid");
+const monk = require("monk");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
+
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required(),
+});
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get("urls");
 
 const app = express();
 
@@ -14,20 +29,43 @@ app.use(express.json());
 
 app.use(express.static("./public"));
 
-// app.get("/url", (req, res) => {
-//   res.status(200).json({ status: "200", data: "hey this the homepage" });
-//   //TODO: create a short url
-// });
+// create short url
+app.post("/url", async (req, res, next) => {
+  let { slug, url } = req.body;
 
-// app.get("/url/:id", (req, res) => {
-//   //TODO: create a short url
-// });
+  try {
+    await schema.validate({ slug, url });
 
-// app.get("*", (req, res) => {
-//   res
-//     .status(404)
-//     .json({ status: "404", data: "OOPs seems like there's nothing here yet" });
-// });
+    if (!slug) {
+      slug = nanoid(5);
+    }
+
+    slug = slug.toLowerCase();
+
+    res.status(200).json({ slug, url });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((error, req, res, next) => {
+  if (error.status) {
+    res.status(error.status);
+  } else {
+    res.status(500);
+  }
+
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? "💥" : error.stack,
+  });
+});
+
+app.get("*", (req, res) => {
+  res
+    .status(404)
+    .json({ status: "404", data: "OOPs seems like there's nothing here yet" });
+});
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
